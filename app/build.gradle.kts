dae43ab9ffd76f6d0d12ab9fe40b28503e7d46c7
Suntrax <qwertzgamer1@gmail.com>
2026-08-18 22:13:15 +0200

    v1.0.1 - changed label to Tensei and adjusted package/namespace

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,9 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     alias(libs.plugins.android.application)
 }
-
-import java.util.Properties
-import java.io.FileInputStream
 
 val keystoreProperties = Properties()
 val keystorePropertiesFile = rootProject.file("local.properties")
@@ -13,15 +13,15 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.blissless.tsukihime"
+    namespace = "com.blissless.tsukihime.anime.torrent"
     compileSdk = 37
 
     defaultConfig {
-        applicationId = "com.blissless.tsukihime"
+        applicationId = "com.blissless.tsukihime.anime.torrent"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     signingConfigs {
